07ab4707804eea7e836fc7e10e0d6a3137ea8a8d
DerekMalone <laxmalone@gmail.com>
2021-07-20 20:59:23 -0500

    adding another key

--- main.js
+++ main.js
@@ -5,26 +5,31 @@ let firstMember = {
     name: "Meseret",
     email: "negash4@gmail.com",
     favColor: "Tumeric",
-    uniqItem: "9 Siblings"
+    uniqItem: "9 Siblings",
+    userImg: "https://avatars.githubusercontent.com/u/86808221?v=4"
 }
 let secondMember = {
     id: 2,
     name: "Grace",
     email: "rgs6090@gmail.com",
     favColor: "Green",
-    uniqItem: "Twin"
+    uniqItem: "Twin",
+    userImg:"https://avatars.githubusercontent.com/u/78558344?v=4"
 }
 let thirdMember = {
     id: 3,
     name: "Derek B",
     email: "dbaylor@gmail.com",
     favColor: "Green",
-    uniqItem: "Excelent Vision"
+    uniqItem: "Excelent Vision",
+    userImg: "https://avatars.githubusercontent.com/u/83030603?v=4"
 }
 let fourthMember = {
     id: 4,
     name: "Derek M",
     email: "laxmalone@gmail.com",
     favColor: "Blue",
-    uniqItem: "Arthritic Knee's"
+    uniqItem: "Arthritic Knee's",
+    userImg: "https://avatars.githubusercontent.com/u/77118878?v=4"
 }
+

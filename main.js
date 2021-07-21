fa66db374233b548083cc92da3673b6e949b808b
DerekMalone <laxmalone@gmail.com>
2021-07-20 21:17:56 -0500

    turning the object into an array

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('I work');
 
-let firstMember = {
+/* let firstMember = {
     id: 1,
     name: "Meseret",
     email: "negash4@gmail.com",
@@ -31,5 +31,39 @@ let fourthMember = {
     favColor: "Blue",
     uniqItem: "Arthritic Knee's",
     userImg: "https://avatars.githubusercontent.com/u/77118878?v=4"
-}
+} */
 
+let group5 = [
+    {
+        id: 1,
+        name: "Meseret",
+        email: "negash4@gmail.com",
+        favColor: "Tumeric",
+        uniqItem: "9 Siblings",
+        userImg: "https://avatars.githubusercontent.com/u/86808221?v=4",
+    }
+    {
+        id: 2,
+        name: "Grace",
+        email: "rgs6090@gmail.com",
+        favColor: "Green",
+        uniqItem: "Twin",
+        userImg:"https://avatars.githubusercontent.com/u/78558344?v=4",
+    }
+    {
+        id: 3,
+        name: "Halie",
+        email: "halie@email.com",
+        favoriteColor: "green",
+        somethingUnique: "allergic to advil",
+        userImg: "https://avatars.githubusercontent.com/u/86806913?v=4", 
+    }
+    {
+        id: 4,
+        name: "Klay",
+        email: "klaythacker@email.com",
+        favoriteColor: "purple",
+        somethingUnique: "has hearing loss",
+        userImg: "https://avatars.githubusercontent.com/u/86626953?v=4",
+    }
+]

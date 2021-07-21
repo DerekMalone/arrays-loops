f0e32a6bc846688c380c89fe19b8d0bfdcc58281
DerekMalone <laxmalone@gmail.com>
2021-07-20 20:49:21 -0500

    Creating objects for each group member

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('I work');
+
+let firstMember = {
+    id: 1,
+    name: "Meseret",
+    email: "negash4@gmail.com",
+    favColor: "Tumeric",
+    uniqItem: "9 Siblings"
+}
+let secondMember = {
+    id: 2,
+    name: "Grace",
+    email: "rgs6090@gmail.com",
+    favColor: "Green",
+    uniqItem: "Twin"
+}
+let thirdMember = {
+    id: 3,
+    name: "Derek B",
+    email: "dbaylor@gmail.com",
+    favColor: "Green",
+    uniqItem: "Excelent Vision"
+}
+let fourthMember = {
+    id: 4,
+    name: "Derek M",
+    email: "laxmalone@gmail.com",
+    favColor: "Blue",
+    uniqItem: "Arthritic Knee's"
+}

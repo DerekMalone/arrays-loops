826b51c3683ac6c33a5e5c7d4ec9179b5ceddfb7
DerekMalone <laxmalone@gmail.com>
2021-07-24 14:03:36 -0500

    worked on serious manipulation of the DOM

---
+++ js-102.js
@@ -0,0 +1,36 @@
+// const mainButton = document.getElementById('main-btn');
+
+// console.log(mainButton);
+
+// const mainButton = document.querySelector('#main-btn')
+
+// const changeText = (e) => {
+//     if (event.target.id === "main-btn") {
+        
+//         document.querySelector('#text').innerHTML = "you clicked that button"
+//     }
+
+//     console.log(e.target.id);
+// }
+
+// mainButton.addEventListener('click', changeText)
+
+// console.log(mainButton);
+
+// const itemOne = (funny) => {
+//     return `Why did the ${funny} cross the road?`
+// }
+
+// const itemTwo = (action) => {
+//     return `How many ${action} does it take to get to the center of a tootsie pop?`
+// }
+
+// const randoQ = (thing, cb) => {
+//     return cb(thing);
+// }
+
+// console.log("chicken", itemTwo);
+
+// const wholeBody = document.querySelector('body');
+
+// wholeBody.addEventListener('click', changeText);
--- main.js
+++ main.js
@@ -1,98 +1,102 @@
-//console.log('I work');
+// //console.log('I work');
 
-/* let firstMember = {
-    id: 1,
-    name: "Meseret",
-    email: "negash4@gmail.com",
-    favColor: "Tumeric",
-    uniqItem: "9 Siblings",
-    userImg: "https://avatars.githubusercontent.com/u/86808221?v=4"
-}
-let secondMember = {
-    id: 2,
-    name: "Grace",
-    email: "rgs6090@gmail.com",
-    favColor: "Green",
-    uniqItem: "Twin",
-    userImg:"https://avatars.githubusercontent.com/u/78558344?v=4"
-}
-let thirdMember = {
-    id: 3,
-    name: "Derek B",
-    email: "dbaylor@gmail.com",
-    favColor: "Green",
-    uniqItem: "Excelent Vision",
-    userImg: "https://avatars.githubusercontent.com/u/83030603?v=4"
-}
-let fourthMember = {
-    id: 4,
-    name: "Derek M",
-    email: "laxmalone@gmail.com",
-    favColor: "Blue",
-    uniqItem: "Arthritic Knee's",
-    userImg: "https://avatars.githubusercontent.com/u/77118878?v=4"
-} */
+// /* let firstMember = {
+//     id: 1,
+//     name: "Meseret",
+//     email: "negash4@gmail.com",
+//     favColor: "Tumeric",
+//     uniqItem: "9 Siblings",
+//     userImg: "https://avatars.githubusercontent.com/u/86808221?v=4"
+// }
+// let secondMember = {
+//     id: 2,
+//     name: "Grace",
+//     email: "rgs6090@gmail.com",
+//     favColor: "Green",
+//     uniqItem: "Twin",
+//     userImg:"https://avatars.githubusercontent.com/u/78558344?v=4"
+// }
+// let thirdMember = {
+//     id: 3,
+//     name: "Derek B",
+//     email: "dbaylor@gmail.com",
+//     favColor: "Green",
+//     uniqItem: "Excelent Vision",
+//     userImg: "https://avatars.githubusercontent.com/u/83030603?v=4"
+// }
+// let fourthMember = {
+//     id: 4,
+//     name: "Derek M",
+//     email: "laxmalone@gmail.com",
+//     favColor: "Blue",
+//     uniqItem: "Arthritic Knee's",
+//     userImg: "https://avatars.githubusercontent.com/u/77118878?v=4"
+// } */
 
-let group5 = [
-    {
-        id: 1,
-        name: "Meseret",
-        favNumber: 12,
-        email: "negash4@gmail.com",
-        favColor: "Tumeric",
-        uniqItem: "9 Siblings",
-        userImg: "https://avatars.githubusercontent.com/u/86808221?v=4",
-    },
-    {
-        id: 2,
-        name: "Grace",
-        favNumber: 56,
-        email: "rgs6090@gmail.com",
-        favColor: "Green",
-        uniqItem: "Twin",
-        userImg:"https://avatars.githubusercontent.com/u/78558344?v=4",
-    },
-    {
-        id: 3,
-        name: "Halie",
-        favNumber: 33,
-        email: "halie@email.com",
-        favoriteColor: "green",
-        somethingUnique: "allergic to advil",
-        userImg: "https://avatars.githubusercontent.com/u/86806913?v=4", 
-    },
-    {
-        id: 4,
-        name: "Klay",
-        favNumber: 56,
-        email: "klaythacker@email.com",
-        favoriteColor: "purple",
-        somethingUnique: "has hearing loss",
-        userImg: "https://avatars.githubusercontent.com/u/86626953?v=4",
-    }
-]
+// let group5 = [
+//     {
+//         id: 1,
+//         name: "Meseret",
+//         favNumber: 12,
+//         email: "negash4@gmail.com",
+//         favColor: "Tumeric",
+//         uniqItem: "9 Siblings",
+//         userImg: "https://avatars.githubusercontent.com/u/86808221?v=4",
+//     },
+//     {
+//         id: 2,
+//         name: "Grace",
+//         favNumber: 56,
+//         email: "rgs6090@gmail.com",
+//         favColor: "Green",
+//         uniqItem: "Twin",
+//         userImg:"https://avatars.githubusercontent.com/u/78558344?v=4",
+//     },
+//     {
+//         id: 3,
+//         name: "Halie",
+//         favNumber: 33,
+//         email: "halie@email.com",
+//         favoriteColor: "green",
+//         somethingUnique: "allergic to advil",
+//         userImg: "https://avatars.githubusercontent.com/u/86806913?v=4", 
+//     },
+//     {
+//         id: 4,
+//         name: "Klay",
+//         favNumber: 56,
+//         email: "klaythacker@email.com",
+//         favoriteColor: "purple",
+//         somethingUnique: "has hearing loss",
+//         userImg: "https://avatars.githubusercontent.com/u/86626953?v=4",
+//     }
+// ]
 
 
-// const filterFavColor = (array, color) => {
-//         return array.filter(taco => taco.favoriteColor === color)
+// // const filterFavColor = (array, color) => {
+// //         return array.filter(taco => taco.favoriteColor === color)
+// // }
+
+// // const filterFavNum = (array, number) => {
+// //         return array.filter(taco => taco.favNumber === number)
+// // }
+
+// // console.log(filterFavNum (group5, 56));
+
+
+
+// //console.log(filterAll (group5, "favNumber", 56));
+
+// const filterAll = (array = null, key = null, item = null) => { //Setting arguments to null helps programers find errors
+//     if (array === null, key === null, item === null) {
+//          return "Need arguments passed."
+//     }else{
+//         return array.filter(taco => taco[key] === item); 
+//     }   
 // }
 
-// const filterFavNum = (array, number) => {
-//         return array.filter(taco => taco.favNumber === number)
-// }
+// // console.log(filterAll(group5));
 
-// console.log(filterFavNum (group5, 56));
+// const divElement = document.querySelector('#main')
 
-
-
-//console.log(filterAll (group5, "favNumber", 56));
-
-const filterAll = (array = null, key = null, item = null) => { //Setting arguments to null helps programers find errors
-    if (array === null, key === null, item === null) {
-         return "Need arguments passed."
-    }else{
-        return array.filter(taco => taco[key] === item); 
-    }   
-}
-
-console.log(filterAll(group5));
+// divElement.innerHTML = "Adding Text";

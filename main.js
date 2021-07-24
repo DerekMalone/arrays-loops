6d98a64e3c97861e87bbb3629405de2ffd7bb97a
DerekMalone <laxmalone@gmail.com>
2021-07-24 10:32:43 -0500

    created a filter with global use

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('I work');
+//console.log('I work');
 
 /* let firstMember = {
     id: 1,
@@ -37,33 +37,62 @@ let group5 = [
     {
         id: 1,
         name: "Meseret",
+        favNumber: 12,
         email: "negash4@gmail.com",
         favColor: "Tumeric",
         uniqItem: "9 Siblings",
         userImg: "https://avatars.githubusercontent.com/u/86808221?v=4",
-    }
+    },
     {
         id: 2,
         name: "Grace",
+        favNumber: 56,
         email: "rgs6090@gmail.com",
         favColor: "Green",
         uniqItem: "Twin",
         userImg:"https://avatars.githubusercontent.com/u/78558344?v=4",
-    }
+    },
     {
         id: 3,
         name: "Halie",
+        favNumber: 33,
         email: "halie@email.com",
         favoriteColor: "green",
         somethingUnique: "allergic to advil",
         userImg: "https://avatars.githubusercontent.com/u/86806913?v=4", 
-    }
+    },
     {
         id: 4,
         name: "Klay",
+        favNumber: 56,
         email: "klaythacker@email.com",
         favoriteColor: "purple",
         somethingUnique: "has hearing loss",
         userImg: "https://avatars.githubusercontent.com/u/86626953?v=4",
     }
 ]
+
+
+// const filterFavColor = (array, color) => {
+//         return array.filter(taco => taco.favoriteColor === color)
+// }
+
+// const filterFavNum = (array, number) => {
+//         return array.filter(taco => taco.favNumber === number)
+// }
+
+// console.log(filterFavNum (group5, 56));
+
+
+
+//console.log(filterAll (group5, "favNumber", 56));
+
+const filterAll = (array = null, key = null, item = null) => { //Setting arguments to null helps programers find errors
+    if (array === null, key === null, item === null) {
+         return "Need arguments passed."
+    }else{
+        return array.filter(taco => taco[key] === item); 
+    }   
+}
+
+console.log(filterAll(group5));
